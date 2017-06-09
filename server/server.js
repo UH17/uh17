45cf692d4060435d31f8d19efd98858449277139
Christoph Madlener <madlener@cs.tum.edu>
2017-06-09 22:36:16 +0200

    Work on server.

--- server/server.js
+++ server/server.js
@@ -1,17 +1,52 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var fs = require('fs');
+var saveFile = '/data/patterns.json';
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'pug');
-var patterns = JSON.parse(fs.readFileSync(__dirname + '/data/patterns.json', 'utf8'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+var data = new Map(JSON.parse(fs.readFileSync(__dirname + saveFile, 'utf8')));
+console.log(data.values());
 
 app.get("/", function(req, res) {
-    res.render('index', { patterns: patterns.patterns});
+    res.render('index', { patterns: Array.from(data.values()) });
+});
+
+app.post("/", function(req, res) {
+    var newPattern = {};
+    newPattern.pattern = req.body.pattern || '';
+    newPattern.name = req.body.name || '';
+    newPattern.url = req.body.url || '';
+    oldPattern = req.body.oldPattern;
+
+    action = req.body.action;
+
+    if (action === 'Delete') {
+	data.delete(oldPattern);
+    }
+    else if (action === 'Save') {
+	if (oldPattern !== newPattern.pattern) {
+	    data.delete(oldPattern);
+	}
+	
+	if (newPattern.pattern !== '' && newPattern.name!== '' && newPattern.url !== '') {
+	    data.set(newPattern.pattern, newPattern);
+	}
+    }
+    
+    res.render('index', { patterns: Array.from(data.values()) });
+    fs.writeFile(__dirname + saveFile, JSON.stringify([...data]));
 });
 
 app.get("/input", function(req, res) {
-    res.render('input');
+    var name = req.query.name || '';
+    var pattern = req.query.pattern || '';
+    var url = req.query.url || '';
+    
+    res.render('input', { name: name, pattern: pattern, url: url });
 });
 
 var port = 8080;
